0b44c0d31f09c79a36d81bf6ca9b81e12bc58bd0
Ben Price <ben.jules.price@gmail.com>
2025-06-16 12:30:11 +0100

    feat: proxy skip first byte

---
+++ src/peripheries/EntryPointV7Proxy.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+import {ENTRY_POINT_V7} from "../types/Constants.sol";
+
+contract EntryPointV7Proxy {
+    fallback() external payable {
+        bytes calldata data;
+        assembly {
+            data.offset := 1
+            data.length := sub(calldatasize(), 1)
+        }
+
+        (bool success, bytes memory result) = ENTRY_POINT_V7.call(data);
+
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+}
