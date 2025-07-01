fa645692dd3d1234cff6ae6709d0b0350aa26225
Ben Price <ben.jules.price@gmail.com>
2025-07-01 13:34:02 +0100

    fix: remove unused gas metering

--- src/peripheries/Simulation.sol
+++ src/peripheries/Simulation.sol
@@ -4,13 +4,7 @@ pragma solidity ^0.8.29;
 import {Delegation} from "../Delegation.sol";
 
 contract Simulation is Delegation {
-    function simulateExecute(bytes32 mode, bytes calldata executionData)
-        external
-        payable
-        returns (uint256)
-    {
-        uint256 gas = gasleft();
+    function simulateExecute(bytes32 mode, bytes calldata executionData) external payable {
         _execute(mode, executionData, true);
-        return gas - gasleft();
     }
 }
