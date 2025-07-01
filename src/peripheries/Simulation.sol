6db76e78404b11e17d3f46b0b4c9c64c0aa52fa1
Ben Price <ben.jules.price@gmail.com>
2025-07-01 13:25:51 +0100

    refactor: move Simulations.sol into peripheries

--- src/peripheries/Simulation.sol
+++ src/peripheries/Simulation.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-import {Delegation} from "./Delegation.sol";
+import {Delegation} from "../Delegation.sol";
 
 contract Simulation is Delegation {
     function simulateExecute(bytes32 mode, bytes calldata executionData)
