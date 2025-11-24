52008c59d1ea8457ff5481f0e1d76552c518e299
Ben Price <ben.jules.price@gmail.com>
2025-11-24 12:39:21 +0000

    feat: ferry contracts (#35)
    * feat: add ferry contracts
    
    * feat: add simulateHandleOps and separate v7 and v8
    
    * fix: handle executeUserOp

--- src/peripheries/EntryPointV8Simulation.sol
+++ src/peripheries/EntryPointV8Simulation.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-import {EntryPoint} from "account-abstraction-v0.8/core/EntryPoint.sol";
-import {ISenderCreator} from "account-abstraction-v0.8/interfaces/ISenderCreator.sol";
 import {PackedUserOperation} from "account-abstraction-v0.8/interfaces/PackedUserOperation.sol";
+import {EntryPoint} from "account-abstraction-v0.8/core/EntryPoint.sol";
+import {ISenderCreator} from "account-abstraction-v0.8/core/SenderCreator.sol";
+import {IAccountExecute} from "account-abstraction-v0.8/interfaces/IAccountExecute.sol";
+
+/* solhint-disable avoid-low-level-calls */
+/* solhint-disable no-inline-assembly */
+/* solhint-disable use-natspec */
+/* solhint-disable gas-increment-by-one */
+/* solhint-disable gas-strict-inequalities */
 
 address constant SENDER_CREATOR = 0x449ED7C3e6Fee6a97311d4b55475DF59C44AdD33;
 
@@ -12,39 +19,103 @@ contract EntryPointV8Simulation is EntryPoint {
         return ISenderCreator(SENDER_CREATOR);
     }
 
-    function simulateHandleOps(PackedUserOperation[] calldata ops, address payable beneficiary)
-        public
+    function simulateHandleOps(PackedUserOperation[] calldata userOps, address payable beneficiary)
+        external
     {
-        uint256 opslen = ops.length;
+        uint256 opslen = userOps.length;
         UserOpInfo[] memory opInfos = new UserOpInfo[](opslen);
 
         unchecked {
             for (uint256 i = 0; i < opslen; i++) {
-                _validatePrepayment(0, ops[i], opInfos[i]);
+                UserOpInfo memory opInfo = opInfos[i];
+                _validatePrepayment(i, userOps[i], opInfo);
             }
 
             uint256 collected = 0;
             emit BeforeExecution();
 
             for (uint256 i = 0; i < opslen; i++) {
-                collected += _executeUserOp(0, ops[i], opInfos[i]);
+                collected += _executeUserOp(i, userOps[i], opInfos[i]);
             }
 
             _compensate(beneficiary, collected);
         }
     }
 
-    function createSenderAndCall(address to, bytes calldata data, bytes calldata initCode)
-        external
+    function validateAccountPrepayment(PackedUserOperation calldata userOp)
+        public
+        returns (UserOpInfo memory outOpInfo)
     {
-        senderCreator().createSender(initCode);
+        uint256 preGas = gasleft();
+        MemoryUserOp memory mUserOp = outOpInfo.mUserOp;
+        _copyUserOpToMemory(userOp, mUserOp);
 
-        if (to != address(0)) {
-            (bool success, bytes memory result) = to.call(data);
+        // getUserOpHash uses temporary allocations, no required after it returns
+        uint256 freePtr = _getFreePtr();
+        outOpInfo.userOpHash = getUserOpHash(userOp);
+        _restoreFreePtr(freePtr);
 
-            if (!success) {
-                assembly {
-                    revert(add(result, 32), mload(result))
+        // Validate all numeric values in userOp are well below 128 bit, so they can safely be added
+        // and multiplied without causing overflow.
+        uint256 verificationGasLimit = mUserOp.verificationGasLimit;
+        uint256 maxGasValues = mUserOp.preVerificationGas | verificationGasLimit
+            | mUserOp.callGasLimit | mUserOp.paymasterVerificationGasLimit
+            | mUserOp.paymasterPostOpGasLimit | mUserOp.maxFeePerGas | mUserOp.maxPriorityFeePerGas;
+        require(maxGasValues <= type(uint120).max, FailedOp(0, "AA94 gas values overflow"));
+
+        uint256 requiredPreFund = _getRequiredPrefund(mUserOp);
+        outOpInfo.prefund = requiredPreFund;
+        _validateAccountPrepayment(0, userOp, outOpInfo, requiredPreFund);
+
+        require(
+            _validateAndUpdateNonce(mUserOp.sender, mUserOp.nonce),
+            FailedOp(0, "AA25 invalid account nonce")
+        );
+
+        unchecked {
+            if (preGas - gasleft() > verificationGasLimit) {
+                revert FailedOp(0, "AA26 over verificationGasLimit");
+            }
+        }
+    }
+
+    function validatePaymasterPrepayment(PackedUserOperation calldata userOp) external {
+        (UserOpInfo memory outOpInfo) = validateAccountPrepayment(userOp);
+
+        _validatePaymasterPrepayment(0, userOp, outOpInfo);
+    }
+
+    function executeUserOp(PackedUserOperation calldata userOp) external {
+        (UserOpInfo memory outOpInfo) = validateAccountPrepayment(userOp);
+
+        if (userOp.paymasterAndData.length > 0) {
+            _validatePaymasterPrepayment(0, userOp, outOpInfo);
+        }
+
+        bytes calldata callData = userOp.callData;
+        if (callData.length > 0) {
+            bytes4 selector;
+            assembly {
+                let len := callData.length
+                if gt(len, 3) { selector := calldataload(callData.offset) }
+            }
+
+            if (selector == IAccountExecute.executeUserOp.selector) {
+                bytes memory executeUserOpCallData =
+                    abi.encodeCall(IAccountExecute.executeUserOp, (userOp, outOpInfo.userOpHash));
+
+                (bool success, bytes memory data) = userOp.sender.call(executeUserOpCallData);
+                if (!success) {
+                    assembly {
+                        revert(add(data, 32), mload(data))
+                    }
+                }
+            } else {
+                (bool success, bytes memory data) = userOp.sender.call(callData);
+                if (!success) {
+                    assembly {
+                        revert(add(data, 32), mload(data))
+                    }
                 }
             }
         }
